328b7fb4f7efc02e890ec8834353638f8315f5f1
Scott Norris <snorris@snorris-a01.vmware.com>
2019-10-21 14:25:27 +1100

    changed to new react front end

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const forecast = require('./utils/forecast')
 const app = express();
 const logResponseTime = require("./utils/response-time-logger");
 const PORT = process.env.WEATHER_FORECAST_LB_SERVICE_PORT || 3002
+var cors = require('cors');
 
+app.use(cors());
 //const latitude = process.env.LATITUDE
 //const longitude = process.env.LONGITUDE
 //const location = process.env.LOCATION
